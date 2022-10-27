bd63149dc483af7e56fbe162710ba87e9da181d8
Matias E. Petenatti <matiasodin96@gmail.com>
2022-10-26 21:41:20 -0300

    changes

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,17 @@
-const bcrypt = require("bcrypt");
-const usersRouter = require("express").Router();
-const User = require("../models/user");
+const bcrypt = require('bcrypt');
+const usersRouter = require('express').Router();
+const User = require('../models/user');
 
-usersRouter.get("/", async (request, response) => {
-  const users = await User.find({}).populate("blogs", {
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({}).populate('blogs', {
     title: 1,
     author: 1,
+    url: 1,
   });
   response.json(users);
 });
 
-usersRouter.post("/", async (request, response) => {
+usersRouter.post('/', async (request, response) => {
   const body = request.body;
 
   const saltRounds = 10;
